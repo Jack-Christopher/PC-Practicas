c9881cf8c182948e356c1bfb1d5a5c71fec6ab9e
Jack <jhuaihuah@unsa.edu.pe>
2021-09-25 16:12:18 -0500

    LC-Display ADDED

--- Lista 2/LC-Display.py
+++ Lista 2/LC-Display.py
@@ -1,3 +1,12 @@
+import sys
+
+def print_numbers(number):
+    for i in range(len (number)):
+        for j in range(len (number[i])):
+            print(number[i][j], end='')
+        print()
+
+
 space = [
     [' '],
     [' '],
@@ -16,9 +25,9 @@ numbers =[
     ],
     [
         [' ', ' ', ' ' ],
-        [' ', '|', ' ' ],
+        [' ', ' ', '|' ],
         [' ', ' ', ' ' ],
-        [' ', '|', ' ' ],
+        [' ', ' ', '|' ],
         [' ', ' ', ' ' ]
     ],
     [
@@ -79,43 +88,67 @@ numbers =[
     ]
 ]
 
-def increment_number(number_to_print, s):
-    col = s+2
-    row = 2*s+3
-    number_to_print = numbers[number_to_print]
-    print_number(number_to_print)
-    number_ = number_to_print[0]
-    for i in range(s):
-        number_.append(number_to_print[1])
+def get_wider(number_to_print, col):   
+    result = []
+    for n_line in range(5):
+        line = []
+        line.append(number_to_print[n_line][0])
+        for n_col in range(col - 2):
+            line.append(number_to_print[n_line][1])
+        line.append(number_to_print[n_line][2])
+        result.append(line)           
         
+    return result
+
+def get_higher(number_to_print, row):
+    result = []
+    result.append(number_to_print[0])
+    for n_line in range((row - 3)//2):
+        result.append(number_to_print[1])
+    result.append(number_to_print[2])
+    for n_line in range((row - 3)//2):
+        result.append(number_to_print[3])
+    result.append(number_to_print[4])
+
+    return result
+
     
+    
+def draw_number(number, s):
+    row = 2*s + 3
+    col = s + 2
+    
+    number_to_print = numbers[number]
+    number_to_print = get_wider(number_to_print, col)
+    number_to_print = get_higher(number_to_print, row)
+    
+    # print_numbers(number_to_print)
 
     return number_to_print
 
+
+
+def join_numbers(lista):
+    result = []
+    for row in range(len(lista[0])):
+        line = []
+        for number in lista:
+            line += number[row]
+            line += [" "]
+        line = line[:-1]
+        result.append(line)
     
+    return result
 
 
+for line in sys.stdin:
+    line = line.replace("\n", "")
+    if line == "0 0":
+        break
+    [s, num ] = [x for x in line.split()]
+    numbers_to_print = [ int(x) for x in num]
+    numbers_to_print =  [draw_number(x, int(s)) for x in numbers_to_print]
+    numbers_to_print = join_numbers(numbers_to_print)
+    print_numbers(numbers_to_print)
+    print()
 
-
-def draw_number(number, s):
-    col = s
-    row = 2*s+3
-
-    number_to_print= increment_number(number, s)
-
-
-    for k in range(row):
-        for m in range(col):
-            print(number_to_print[k][m], end='')
-        print()
-
-
-# draw_number(int(input()), 1)
-
-def print_number(number):
-    for i in range(len (number)):
-        for j in range(len (number[i])):
-            print(number[i][j], end='')
-        print()
-
-print_number(increment_number(int(input()), 2))
