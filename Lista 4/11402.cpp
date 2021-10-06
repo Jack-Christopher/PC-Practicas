162ce2d25d9b0feeeffd918a52ee2dfefbb0525b
Jack <jhuaihuah@unsa.edu.pe>
2021-10-06 10:38:30 -0500

    11402 ADDED with TLE (second time)

--- Lista 4/11402.cpp
+++ Lista 4/11402.cpp
@@ -1,116 +1,52 @@
+#include <bits/stdc++.h> 
 #include <iostream>
 #include <vector>
+#include <bitset>
 #include <math.h>
 
-class SumSegmentTree
+
+void change(std::bitset<1024000> *bs, int a, int b, int option)
 {
-private:
-    int n;
-    std::vector<int> tree;
-    std::vector<bool> arreglo;
+    int longitud = b-a+1;
+    std::string init (longitud, '1');        
+    std::bitset<1024000> aux (init);
+    // std::cout<< "aux: " << aux << std::endl;
 
-public:
-    SumSegmentTree(std::vector<bool> &arreglo)
+    if (option == 1)
     {
-        this->arreglo = arreglo;
-        n = arreglo.size();
-        int x = (int)(ceil(log2(n)));
-        int max_size = 2*(int)pow(2, x) - 1;
-        tree.resize(max_size);
-        build(0, 0, n - 1);
+        aux<<= a;
+        // std::cout<< "aux: " << aux << std::endl;
+        *bs |= aux;
+        // std::cout<< "after OR: \n";
     }
-
-    void build(int node, int start, int end)
+    else if ( option == 0)
     {
-        if (start == end)
-        {
-            tree[node] = arreglo[start];
-            return;
-        }
-        int mid = (start + end) / 2;
-        build(2 * node+1, start, mid);
-        build(2 * node + 2, mid + 1, end);
-        tree[node] = tree[2 * node+1] + tree[2 * node + 2];
+        aux<<= a;
+        aux = ~aux;
+        // std::cout<< "aux: " << aux << std::endl;
+        *bs &= aux;
+        // std::cout<< "after AND: \n";
     }
-
-    int query(int node, int start, int end, int i, int j)
-    {
-        if (start > j || end < i)
-            return 0;
-        if (start >= i && end <= j)
-            return tree[node];
-        int mid = (start + end) / 2;
-        return query(2 * node+1, start, mid, i, j) + query(2 * node + 2, mid + 1, end, i, j);
+    else if (option == -1)
+    {   
+        aux<<= a;
+        // std::cout<< "aux: " << aux << std::endl;
+        *bs ^= aux;
+        // std::cout<< "after XOR: \n";
     }
+    // std::cout << bs->to_string() << "\n\n";
+}
 
-    int query (int start, int end)
-    {
-        return query(0, 0, n - 1, start, end);
-    }
-
-    void update(int node, int start, int end, int i, int val)
-    {
-        if (start > i || end < i)
-            return;
-        if (start == end)
-        {
-            tree[node] = val;
-            return;
-        }
-        int mid = (start + end) / 2;
-        update(2 * node+1, start, mid, i, val);
-        update(2 * node + 2, mid + 1, end, i, val);
-        tree[node] = tree[2 * node+1] + tree[2 * node + 2];
-    }
-
-    // option 0: change all elements in range to cero
-    // option 1: change all elements in range to one
-    // option 2: change all elements in range to its inverse (0 -> 1, 1 -> 0)
-
-    void update_range(int start, int end, int option)
-    {
-        for (int k = start; k <= end; k++)
-        {
-            if (option == 0)
-                arreglo[k] = 0;
-            else if (option == 1)
-                arreglo[k] = 1;
-            else if (option == 2)
-                arreglo[k] = !arreglo[k];
-            update(0, 0, n - 1, k, arreglo[k]);
-        }
-        
-    }
-
-    // void print()
-    // {
-    //     for (int i = 0; i < tree.size(); i++)
-    //         std::cout << tree[i] << " ";
-    //     std::cout << std::endl;
-    // }
-    // void print_array()
-    // {
-    //     int i, j, h = std::ceil(log2(n));
-    //     for(i=0 ; i<=h ; ++i)
-    //     {
-    //         for(j=0 ; j<pow(2, i) ; ++j)
-    //             std::cout<<tree[int(pow(2, i)-1 + j)]<<' ';
-    //         std::cout<<std::endl;
-    //     }
-    // }
-};
-
-std::vector<bool> fromStringToVector(std::string s)
+int count( std::bitset<1024000> *bs, int a, int b)
 {
-    std::vector<bool> v;
-    for (int i = 0; i < s.size(); i++)
-    {
-        if (s[i] == '1')
-            v.push_back(true);
-        else
-            v.push_back(false);
-    }
-    return v;
+    int longitud = b-a+1;
+    std::string init (longitud, '1');        
+    std::bitset<1024000> aux (init);
+
+    aux<<= a;
+    aux &= *bs;
+    // std::cout<< "S_aux: " << aux << std::endl;
+    return aux.count();
 }
 
 int main()
@@ -127,7 +63,6 @@ int main()
         // respuestas
         std::string description = "";
         std::vector<int> answers;
-        std::vector<bool> arreglo;
 
         for (int i = 0; i < m; i++)
         {
@@ -138,9 +73,11 @@ int main()
             for (int t = 0; t < T; t++)
                 description += pirate;
         }
-
-        arreglo = fromStringToVector(description);
-        SumSegmentTree sst(arreglo);
+        // std::cout<< "Descripcion: \t" << description << "\n";
+        // 101010101010001000
+        std::reverse(description.begin(), description.end());
+        // std::cout<< "Descripcion: \t" << description << "\n";
+        std::bitset<1024000> arreglo(description);
 
         int Q, a, b;
         char query;
@@ -149,13 +86,13 @@ int main()
         {
             std::cin>> query >> a >> b; 
             if (query == 'F')
-                sst.update_range(a, b, 1);
+                change(&arreglo, a, b, 1);
             else if (query == 'E')
-                sst.update_range(a, b, 0);
+                change(&arreglo, a, b, 0);
             else if (query == 'I')
-                sst.update_range(a, b, 2);
+                change(&arreglo, a, b, -1);
             else if (query == 'S')
-                answers.push_back(sst.query(a, b));
+                answers.push_back(count(&arreglo, a, b));
         }        
         
         std::cout<< "Case "<< k+1<< ":\n";
@@ -165,3 +102,5 @@ int main()
 
     return 0;
 }
+
+
